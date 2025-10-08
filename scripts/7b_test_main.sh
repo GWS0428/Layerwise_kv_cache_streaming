8918f8b58c8c6048a7b1f94093edb55a726457c7
Wooseok Gwak <wooseok428@gmail.com>
2025-10-08 08:58:34 +0000

    Delete unnecessary shell scripts

--- scripts/7b_test_main.sh
+++
@@ -1,9 +0,0 @@
-export MODEL=mistral-community/Mistral-7B-v0.2
-export MODEL_ID=mistral7b
-python main_layerwise.py \
-    --model_id  $MODEL \
-    --save_dir ${SAVE_DIR}/${MODEL_ID}_$1_data/ \
-    --start 0 \
-    --end 1  \
-    --num_gpus 1 \
-    --dataset_name $1
